3542b0607bd3dc63c0daccea6f2bac599a8950c9
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-20 15:23:30 -0300

    Tailwind styling

--- web/main.js
+++ web/main.js
@@ -1,7 +1,7 @@
 (async() => {
     const data = await fetch('../instagram.json').then(response => response.json())
 
-    const htmlList = data.map(({ src }) => `<div><img src="${src}" alt="Yasmim Santos" /></div>`).join('')
+    const htmlList = data.map(({ src }) => `<div class="cursor-pointer transform transition hover:-translate-y-3"><img class="rounded w-44 object-cover" src="${src}" alt="Yasmim Santos" /></div>`).join('')
     console.info(htmlList)
 
     document.getElementsByTagName('main')[0].innerHTML = htmlList
