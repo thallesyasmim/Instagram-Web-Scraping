45af6557d552fc2a60b246d42a78d6a02cc7832a
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-20 14:19:09 -0300

    get source images

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs')
 
-(async () => {
-    const browser = await puppeteer.launch()
+async function start() {
+    const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto('https://www.instagram.com/nhasmim/')
 
 
-    await page.waitForTimeout(5000)
+    const imgList = await page.evaluate(() => {
+        const nodeList = document.querySelectorAll('article.ySN3v img')
+
+        const imgArray = [...nodeList]
+
+        const imgList = imgArray.map(({ src }) => ({ src }))
+
+        return imgList
+    })
 
     await browser.close()
-})()
+
+    console.log(imgList)
+}
+
+start()
