92a2bae922a3273b59ae86306825a7bb2bd2c7c7
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-20 14:58:47 -0300

    using method join()

--- web/main.js
+++ web/main.js
@@ -1,9 +1,8 @@
 (async() => {
     const data = await fetch('../instagram.json').then(response => response.json())
 
-    const htmlList = data.map(({ src }) => `<div><img src="${src}" alt="Yasmim Santos" /></div>`)
+    const htmlList = data.map(({ src }) => `<div><img src="${src}" alt="Yasmim Santos" /></div>`).join('')
     console.info(htmlList)
 
-    htmlList.forEach(htmlElement => document.getElementsByTagName('main')[0].innerHTML += htmlElement)
-    
+    document.getElementsByTagName('main')[0].innerHTML = htmlList
 })()
